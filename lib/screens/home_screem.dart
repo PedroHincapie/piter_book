2c83d843ce9b6d6d8b61218c2a4dd078a9015658
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-08-02 08:19:10 -0500

    Update home_screem.dart

--- lib/screens/home_screem.dart
+++ lib/screens/home_screem.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:piter_book/widgets/custom_tab_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -78,9 +79,83 @@ class _HomeScreenState extends State<HomeScreen> {
                             'assets/svg/background_search.svg',
                           ),
                         ),
+                        Positioned(
+                          right: 9.0,
+                          top: 8.0,
+                          child: SvgPicture.asset(
+                            'assets/icons/icons_search_white.svg',
+                          ),
+                        ),
                       ],
                     ),
-                  )
+                  ),
+                  Container(
+                    height: 25.0,
+                    margin: EdgeInsets.only(top: 30.0),
+                    padding: EdgeInsets.only(left: 25.0),
+                    child: DefaultTabController(
+                      length: 3,
+                      child: TabBar(
+                        labelPadding: EdgeInsets.all(0.0),
+                        indicatorPadding: EdgeInsets.all(0.0),
+                        isScrollable: true,
+                        labelColor: Color(0xFF121212),
+                        unselectedLabelColor: Color(0xFFAAAAAA),
+                        labelStyle: GoogleFonts.openSans(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                        unselectedLabelStyle: GoogleFonts.openSans(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        indicator: RoundedRectangleTabIndicator(
+                          weight: 2.0,
+                          width: 10.0,
+                          color: Color(0xFF121212),
+                        ),
+                        tabs: [
+                          Tab(
+                            child: Container(
+                              child: Text('New'),
+                              margin: EdgeInsets.only(right: 23),
+                            ),
+                          ),
+                          Tab(
+                            child: Container(
+                              margin: EdgeInsets.only(right: 23),
+                              child: Text('Trending'),
+                            ),
+                          ),
+                          Tab(
+                            child: Container(
+                              margin: EdgeInsets.only(right: 23),
+                              child: Text('Best Selier'),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 21),
+                    height: 210.0,
+                    color: Colors.red,
+                    child: ListView.builder(
+                        padding: EdgeInsets.only(left: 25.0, right: 6.0),
+                        itemCount: 2,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: EdgeInsets.only(right: 10.0),
+                            height: 210.0,
+                            width: 153.0,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                image: DecorationImage(
+                                  image: AssetImage('assets/images/book_1.png'),)),
+                          );
+                        }),
+                  ),
                 ],
               ),
             )
